5f2f5fb8d9ef90d3141ad7768590015796b5aba6
Almaz <67281187+DAD609@users.noreply.github.com>
2024-05-26 18:00:54 +0300

    Create 5. Вторая по величине зарплата.sql

---
+++ Средний/5. Вторая по величине зарплата.sql
@@ -0,0 +1,9 @@
+SELECT 
+    COALESCE(
+        (SELECT DISTINCT salary
+         FROM Employee
+         ORDER BY salary DESC
+         OFFSET 1 ROW
+         FETCH NEXT 1 ROW ONLY), 
+        NULL
+    ) AS SecondHighestSalary;
